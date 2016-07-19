21988a095a4eafa747798b9ca4788369a62c2b28
Will James <jameswt@gmail.com>
2016-07-19 16:32:08 +1000

    change max age to a day... will probably want to make this a lot more robust for production since any change in ncaa website will bring down the site.

--- src/util/memoize.js
+++ src/util/memoize.js
@@ -3,7 +3,7 @@ var memoizee = require('memoizee'),
 
 
 function memoize(fn) {
-  return memoizee(fn, { maxAge: 60*60*1000 });
+  return memoizee(fn, { maxAge: 24*60*60*1000 });
 }
 
 module.exports = memoize;
